77145f17b57c2b6f6a1dc0c552b05d412074158b
ish-u <anmolgupta520@gmail.com>
2022-10-19 19:44:50 +0530

    added configs for passport and express-session

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,10 @@ dotenv.config();
 import express from "express";
 import { Express } from "express";
 import cors from "cors";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import passport from "passport";
+import "./config/session";
 
 // Express App
 const app: Express = express();
@@ -22,6 +26,25 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URI || "",
+    }),
+    proxy: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      secure: process.env.DEV === "TRUE" ? false : true,
+      sameSite: process.env.DEV === "TRUE" ? "lax" : "none",
+      httpOnly: true,
+    },
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use("/public", express.static("public"));
 
 export default app;
