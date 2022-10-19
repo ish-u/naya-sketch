82c3122e7b5c007f5e58920da6827d385e82d320
ish-u <anmolgupta520@gmail.com>
2022-10-19 19:44:12 +0530

    added /login, /register, /logout, /check routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,23 @@
 import { Express } from "express";
+import passport from "passport";
 import { indexHandler } from "./controllers/index.controller";
+import { authenticate } from "./utils/authenticate";
+import {
+  checkSessionHandler,
+  registerHandler,
+  loginHandler,
+  logoutHandler,
+} from "./controllers/user.controller";
 
 function routes(app: Express) {
   app.get("/", indexHandler);
+  // SESSION
+  app.get("/check", authenticate, checkSessionHandler);
+
+  // USER
+  app.post("/login", passport.authenticate("local"), loginHandler);
+  app.post("/register", registerHandler);
+  app.get("/logout", authenticate, logoutHandler);
 }
 
 export default routes;
