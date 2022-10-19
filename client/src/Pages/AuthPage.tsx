a5e4c37214073c13fce29c2a107f823b6dbf2ee3
ish-u <anmolgupta520@gmail.com>
2022-10-19 23:00:13 +0530

    created AuthPage

--- client/src/Pages/AuthPage.tsx
+++ client/src/Pages/AuthPage.tsx
@@ -1,7 +1,25 @@
-const AuthPage = () => {
-  return (
-    <div>AuthPage</div>
-  )
-}
+import { useState } from "react";
+import Login from "../Components/Login";
+import Register from "../Components/Register";
 
-export default AuthPage
+const AuthPage = ({
+  setIsAuthenticated,
+}: {
+  setIsAuthenticated: (value: boolean) => void;
+}) => {
+  const [current, setCurrent] = useState<"login" | "register">("login");
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      {current === "login" && (
+        <Login
+          setIsAuthenticated={setIsAuthenticated}
+          setCurrent={setCurrent}
+        />
+      )}
+      {current === "register" && <Register setCurrent={setCurrent} />}
+    </div>
+  );
+};
+
+export default AuthPage;
