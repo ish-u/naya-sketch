28d614a3bc21ce3efcbe3cfd1d4341bfc84085a4
ish-u <anmolgupta520@gmail.com>
2022-10-19 23:56:48 +0530

    added AuthCheck, Loading

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,39 @@
-import Canvas from "./Components/Canvas";
+import { useEffect, useState } from "react";
+import NavBar from "./Components/NavBar";
+import AuthPage from "./Pages/AuthPage";
+import SketchPage from "./Pages/SketchPage";
 const App = () => {
-  return (
-    <div className="">
-      <Canvas />
-    </div>
-  );
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const check = await fetch(import.meta.env.VITE_APP_API + "/check", {
+        credentials: "include",
+      });
+      if (check.status === 201) {
+        setIsAuthenticated(true);
+      }
+      setLoading(false);
+    })();
+  }, []);
+
+  if (loading) {
+    return <>Loading</>;
+  } else if (isAuthenticated) {
+    return (
+      <>
+        <NavBar />
+        <SketchPage />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <AuthPage setIsAuthenticated={setIsAuthenticated} />
+      </>
+    );
+  }
 };
 
 export default App;
