82491defd2e006e43745021a7a6baebd63a49b72
ish-u <anmolgupta520@gmail.com>
2022-10-21 23:19:41 +0530

    added pulse animation

--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      pulse: "pulse 2s ease-in-out infinite",
+    },
+    keyframes: {
+      pulse: {
+        "0%": {
+          transform: "scale(0.5)",
+        },
+        "50%": {
+          transform: "scale(1)",
+        },
+        "100%": {
+          transform: "scale(0.5)",
+        },
+      },
+    },
   },
   plugins: [],
 };
