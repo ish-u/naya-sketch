360acf786294e7aadb460c630766a44b92361448
ish-u <anmolgupta520@gmail.com>
2022-10-23 00:38:44 +0530

    refactor

--- server/src/socket.ts
+++ server/src/socket.ts
@@ -13,7 +13,6 @@ const io = new Server(socketIOServer, {
     ],
     credentials: true,
   },
-  path: "/socket.io/",
 });
 
 appIO.get("/", (req: Request, res: Response) => {
@@ -21,37 +20,37 @@ appIO.get("/", (req: Request, res: Response) => {
 });
 
 io.on("connection", (socket) => {
-  console.log("Connected");
+  //console.log("Connected");
   // join a room
   socket.on("join-room", async ({ room, username }) => {
-    console.log(`${socket.id}//${username} joined ${room}`);
+    //console.log(`${socket.id}//${username} joined ${room}`);
     socket.join(room);
     socket.to(room).emit("add-user", { username: username });
   });
 
   // leave a room
   socket.on("leave-room", async ({ room, username }) => {
-    console.log(`${socket.id}//${username}  left ${room}`);
+    // console.log(`${socket.id}//${username}  left ${room}`);
     socket.to(room).emit("remove-user", { username: username });
     socket.leave(room);
   });
 
   // update-collaborators
   socket.on("update-me", ({ room, username }) => {
-    console.log("send-status", room, username);
+    //console.log("send-status", room, username);
     socket.to(room).emit("update-user", { username: username });
   });
 
   socket.on("send-points", async ({ points, room, username }) => {
     socket.to(room).emit("get-points", {
-      collaboratersPoints: points,
+      collaboratorsPoints: points,
       username: username,
     });
   });
 
   // create a new sketch
   socket.on("new-sketch", ({ sketchName, username }) => {
-    console.log(sketchName, username);
+    //console.log(sketchName, username);
     socket.broadcast.emit("add-sketch", {
       newSketch: sketchName,
       username: username,
@@ -60,7 +59,7 @@ io.on("connection", (socket) => {
 
   // disconnect
   socket.on("disconnect", () => {
-    console.log("Disconnected");
+    //console.log("Disconnected");
   });
 });
 
